316adc685fe4f263e80738ecc3c8f7515c10966a
Chaitra <chaitradangat084@gmail.com>
2019-03-31 20:32:55 +0530

    Push Server Class

---
+++ PushServer/Interfaces/IPushServer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PushServer.Interfaces
+{
+    public interface IPushServer
+    {
+        string SubscribeClient(string ClientInfo);
+
+        string UnSubscribeClient(string ClientInfo);
+
+        bool ReceiveChanges(string ClientInfo,string ClientData);
+
+        string SendPushNotification();
+    }
+}
---
+++ PushServer/PushServer.cs
@@ -0,0 +1,27 @@
+﻿using PushServer.Interfaces;
+
+namespace PushServer
+{
+    class PushServer : IPushServer
+    {
+        public bool ReceiveChanges(string ClientInfo, string ClientData)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string SendPushNotification()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string SubscribeClient(string ClientInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string UnSubscribeClient(string ClientInfo)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
